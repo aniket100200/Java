b24a1287e85ba2873cc632f7ed94bc951ad69924
aniket100200 <116418804+aniket100200@users.noreply.github.com>
2023-03-15 17:01:38 +0530

    Create JavaAbstractClass.java

---
+++ Module2/OOPs/JavaAbstractClass.java
@@ -0,0 +1,42 @@
+import java.util.*;
+
+abstract class Book
+{
+	String title;
+	abstract void setTitle(String s);
+	String getTitle()
+	{
+		return title;
+	}
+}
+
+//Write MyBook class here
+class MyBook extends Book
+{
+	public void setTitle(String s)
+	{
+		super.title=s;
+	}
+	MyBook()
+	{
+		//code her...
+	}
+	public String getTitle()
+	{
+		return super.title;
+	}
+
+}
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String title = sc.nextLine();
+        MyBook new_novel = new MyBook();
+        new_novel.setTitle(title);
+        System.out.println("The title is: " + new_novel.getTitle());
+        sc.close();
+
+    }
+}
