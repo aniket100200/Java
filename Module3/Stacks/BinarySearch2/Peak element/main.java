f6a02dab1b2c550341634bdfb746916e0efe7a1b
aniket100200 <116418804+aniket100200@users.noreply.github.com>
2023-03-24 13:35:22 +0530

    Create main.java

---
+++ Module3/Stacks/BinarySearch2/Peak element/main.java
@@ -0,0 +1,116 @@
+import java.util.*;
+
+class Solution{
+    public int findPeak(int arr[], int n)
+    {
+       // Your code here.
+		//let's have a search space...
+		
+		int start=0;
+		int end=n-1;
+		if(n==1)
+		{
+			return 0;
+		}
+		
+		while(start<=end)
+		{
+			//let's find the mid..
+			int mid=(start+end)/2;
+			if(mid==n-1)
+			{
+				//this is one of the edge case.... if you increasign sorted 
+				//array right andgled triangle..
+				if(arr[mid]>arr[mid-1])
+				{
+					return mid;
+				}
+				else
+				{
+					//move towards the left part...;
+					end=mid-1;
+				}
+			}
+			else if(mid==0)
+			{
+				if(arr[mid]>arr[mid+1])
+				{
+					return mid;
+				}
+				else
+				{
+					//move towards the right... part..
+					start=mid+1;
+				}
+			}
+			else
+			{
+				if(arr[mid]>=arr[mid-1] && arr[mid]>=arr[mid+1])
+				{
+					//if it is peak...
+					return mid;
+				}
+				else if(arr[mid]>=arr[mid-1])
+				{
+					/*means it is in increaseing order 
+					means you are  at left side of the mountain..
+					so to find peek you have to move..
+					towards the right...
+					*/	
+					start=mid+1;
+				}
+				else
+				{
+					//means your in right part of the array 
+					//let's go towards the peek ... means move 
+					//toward the left...
+					end=mid-1;
+				}
+			}
+		}
+		return -1;
+       
+    }
+}
+
+class Main{
+	public static void main(String[] args){
+		try (Scanner sc = new Scanner(System.in)) {
+			int t=sc.nextInt();
+			while(t>0){
+				int n=sc.nextInt();
+				int []A=new int[n];
+				for(int i=0;i<n;i++){
+					A[i]=sc.nextInt();
+				}
+				Solution ob =new Solution();
+				int ans = ob.findPeak(A,n);
+                if(ans==0){
+                    if(A[ans]>A[ans+1]){
+                        System.out.println("true");
+                    }
+                    else{
+                       System.out.println("false");    
+                    }
+                }
+                else if(ans==n-1){
+                    if(A[ans]>A[ans-1]){
+                        System.out.println("true");                        
+                    }
+                    else{
+                        System.out.println("false");                        
+                    }
+                }
+                else{
+                    if(A[ans]>A[ans+1] && A[ans]>A[ans-1]){
+                        System.out.println("true");
+                    }
+                    else{
+                        System.out.println("false");                        
+                    }
+                }
+				t--;
+			}
+		}
+	}
+}
