49b1e4f1d4fc8b599c9a627132357b375b065a70
aniket100200 <116418804+aniket100200@users.noreply.github.com>
2023-03-29 18:14:35 +0530

    Create O(n)&&O(H)Solution.java

---
+++ Module3/Stacks/BinaryTree3/Lowest Common Ancestor/O(n)&&O(H)Solution.java
@@ -0,0 +1,81 @@
+import java.util.LinkedList;
+import java.util.Queue;
+import java.io.*;
+import java.util.*;
+
+
+
+class Main {
+    static Node buildTree(String str) {
+        if (str.length() == 0 || str.charAt(0) == 'N') {
+            return null;
+        }
+        String ip[] = str.split(" ");
+        Node root = new Node(Integer.parseInt(ip[0]));
+        Queue<Node> queue = new LinkedList<>();
+        queue.add(root);
+        int i = 1;
+        while (queue.size() > 0 && i < ip.length) {
+            Node currNode = queue.peek();
+            queue.remove();
+            String currVal = ip[i];
+            if (!currVal.equals("-1")) {
+                currNode.left = new Node(Integer.parseInt(currVal));
+                queue.add(currNode.left);
+            }
+            i++;
+            if (i >= ip.length) break;
+            currVal = ip[i];
+            if (!currVal.equals("-1")) {
+                currNode.right = new Node(Integer.parseInt(currVal));
+                queue.add(currNode.right);
+            }
+            i++;
+        }
+
+        return root;
+    }
+
+    public static void main(String[] args) throws IOException {
+        Scanner sc = new Scanner(System.in);
+        String s = sc.nextLine();
+        Node root = buildTree(s);
+        int x = sc.nextInt();
+        int y = sc.nextInt();
+        Solution g = new Solution();
+        Node ans = g.findLCA(root,x,y);
+        System.out.println(ans.data);
+    }
+}
+
+
+class Node {
+    int data;
+    Node left;
+    Node right;
+    Node(int data) {
+        this.data = data;
+        left = null;
+        right = null;
+    }
+}
+
+class Solution {
+	/*This is with recurrsion O(N) Tc ---> and O(H)..i.e. logn Sc****;/*/
+    public static Node findLCA(Node node,int n1,int n2)
+	{
+       // write code here
+		if(node==null)return null;
+		if(node.data==n1 || node.data ==n2)return node;
+
+		//let's find in the left and right child...
+		Node lc=findLCA(node.left,n1,n2);
+
+		Node rc=findLCA(node.right,n1,n2);
+
+		if(lc!=null && rc!=null)return node;
+		if(lc!=null)return lc;
+		if(rc!=null)return rc;
+		return null;
+    }
+}
