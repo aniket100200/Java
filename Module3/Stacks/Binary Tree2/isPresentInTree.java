6ec9dd7cc5b0239cafc259a2335d00ca26516a6c
aniket100200 <116418804+aniket100200@users.noreply.github.com>
2023-03-29 12:51:28 +0530

    Create isPresentInTree.java

---
+++ Module3/Stacks/Binary Tree2/isPresentInTree.java
@@ -0,0 +1,86 @@
+import java.util.LinkedList; 
+import java.util.Queue; 
+import java.io.*;
+import java.util.*;
+
+class Node
+{
+    int data;
+    Node left;
+    Node right;
+    Node(int data)
+	{
+        this.data = data;
+        left=null;
+        right=null;
+    }
+}
+
+
+class Main {
+    static Node buildTree(String str){
+        if(str.length()==0 || str.charAt(0)=='N'){
+            return null;
+        }
+        String ip[] = str.split(" ");
+        Node root = new Node(Integer.parseInt(ip[0]));
+        Queue<Node> queue = new LinkedList<>(); 
+        queue.add(root);
+        int i = 1;
+        while(queue.size()>0 && i < ip.length) {
+            Node currNode = queue.peek();
+            queue.remove();
+            String currVal = ip[i];
+            if(!currVal.equals("N")) {
+                currNode.left = new Node(Integer.parseInt(currVal));
+                queue.add(currNode.left);
+            }
+            i++;
+            if(i >= ip.length)
+                break;   
+            currVal = ip[i];
+            if(!currVal.equals("N")) {
+                currNode.right = new Node(Integer.parseInt(currVal));
+                queue.add(currNode.right);
+            }
+            i++;
+        }
+        return root;
+    }
+
+    
+	public static void main (String[] args) throws IOException{
+            Scanner sc = new Scanner(System.in);
+            int n=sc.nextInt();
+            sc.nextLine();
+			String s = sc.nextLine();
+            int b=sc.nextInt();
+			Node root = buildTree(s);
+			Solution tree = new Solution();
+			boolean ans=tree.solve(root,b);
+           
+            System.out.print(ans);
+
+            System.out.println();
+
+            sc.close();			
+	}
+}
+  
+
+
+class Solution{
+    
+   public boolean solve(Node root,int b)
+	{
+		if(root==null)return false;
+		//let's try to find in left subtree..
+		if(root.data==b)return true;
+
+		//find in left subtree...
+		if(solve(root.left,b))return true;
+		if(solve(root.right,b))return true;
+		return false;
+          
+    }
+}
