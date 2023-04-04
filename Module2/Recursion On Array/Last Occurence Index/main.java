b4c390b4c2e4f7c414c62465891ab8fed87caecd
Aniket Khangar <116418804+aniket100200@users.noreply.github.com>
2023-04-04 09:40:42 +0530

    Create main.java

---
+++ Module2/Recursion On Array/Last Occurence Index/main.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+
+
+
+class Main {
+	public static void main(String[] args) {
+		Scanner s = new Scanner(System.in);
+        int N = s.nextInt();
+		int[] A = new int[N];
+		for(int i = 0; i < N; i++){
+			A[i] = s.nextInt();
+		}
+		int T = s.nextInt();
+		System.out.println(lastIndex(A, T, N-1));
+	}
+
+	static int lastIndex(int A[],int T,int startIndex)
+	{
+		//Write your code here
+		if(startIndex<0)
+		{
+			return -1;
+		}
+		if(A[startIndex]==T)
+		{
+			return startIndex;
+		}
+		return lastIndex(A,T,--startIndex);
+		
+	}
+}
