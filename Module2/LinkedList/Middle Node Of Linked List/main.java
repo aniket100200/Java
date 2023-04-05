6d325c5d63c4f4e6d87c078ff2f4966b611617c7
Aniket Khangar <116418804+aniket100200@users.noreply.github.com>
2023-04-05 10:02:10 +0530

    Create main.java

---
+++ Module2/LinkedList/Middle Node Of Linked List/main.java
@@ -0,0 +1,101 @@
+import java.util.*;
+
+
+public class Main {
+    public static void main(String[] args) throws Throwable {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        LinkedList llist= new LinkedList(); 
+        int a1=sc.nextInt();
+        Node head= new Node(a1);
+        llist.addToTheLast(head);
+        for (int i = 1; i < n; i++) 
+        {
+            int a = sc.nextInt(); 
+            llist.addToTheLast(new Node(a));
+        }
+            
+        Solution A = new Solution();
+        llist.head = A.midpointOfLinkedList(llist.head);
+        llist.printList();
+        
+    }
+}
+
+class Node
+    {
+        int data;
+        Node next;
+        Node(int d) {data = d; next = null; }
+    }
+    
+class LinkedList
+{
+    Node head;  
+    Node tail;
+    public void addToTheLast(Node node) 
+    {
+      if (head == null) 
+      {
+       head = node;
+       tail = node;
+      } 
+      else 
+      {
+       tail.next = node;
+       tail = node;
+      }
+    }
+      void printList()
+    {
+        Node temp = head;
+        while (temp != null)
+        {
+           System.out.print(temp.data+" ");
+           temp = temp.next;
+        }  
+        System.out.println();
+    }
+    
+}
+
+class Solution
+{
+    static Node midpointOfLinkedList(Node head)
+    {
+  //       //Your code here
+		// //first take count of all nodes
+		// Node temp=head;//temp is pointing the head
+		// int n=0;
+		// while(temp!=null)
+		// {
+		// 	n++;
+		// 	temp=temp.next;
+		// }
+		
+		// //I have counted the node 
+		// //reset the temp
+		// temp=head;
+
+		// for(int i=0;i<n/2;i++)
+		// {
+		// 	temp=temp.next;
+		// }
+		// return temp;	
+
+
+
+		//hare and tortoise mechanism
+
+			Node fast=head;
+		Node slow=head;
+		while(fast!=null && fast.next!=null)
+		{
+			fast=fast.next.next;
+			slow=slow.next;
+		}
+		return slow;
+		
+    }
+    
+}
