53c0fff6b0d811a87ab31506faa8302f1092ed25
Aniket Khangar <116418804+aniket100200@users.noreply.github.com>
2023-04-05 11:35:22 +0530

    Create main.java

---
+++ Module2/LinkedList/Linked Lists 2/Linked list cycle 2/main.java
@@ -0,0 +1,98 @@
+import java.util.*;
+import java.io.*;
+import java.lang.*;
+
+class Node
+{
+    int data;
+    Node next;
+    
+    Node(int x)
+    {
+        data = x;
+        next = null;
+    }
+}
+
+class Main
+{
+    public static void makeLoop(Node head, Node tail, int x){
+        if (x == 0) return;
+        
+        Node curr = head;
+        for(int i=1; i<x; i++)
+            curr = curr.next;
+        
+        tail.next = curr;
+    }
+    
+    public static void main (String[] args){
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        
+        int num = sc.nextInt();
+        Node head = new Node(num);
+        Node tail = head;
+        
+        for(int i=0; i<n-1; i++)
+        {
+            num = sc.nextInt();
+            tail.next = new Node(num);
+            tail = tail.next;
+        }
+        
+        int pos = sc.nextInt();
+        makeLoop(head, tail, pos);
+        
+        Solution x = new Solution();
+        Node y = x.detectLoop(head); 
+        if( y == null )
+            System.out.println(0);
+        else
+            System.out.println(y.data);
+    }
+}
+
+class Solution {
+    public static Node detectLoop(Node head)
+	{
+        //write code here
+		//floyed cycle detection method 
+
+		//here we go
+		Node fast=head;
+		Node slow=head;
+
+		//first find where they meet 
+		//here we go
+		boolean flag=true;
+		while(fast!=null && fast.next!=null)
+		{
+			fast=fast.next.next;
+			slow=slow.next;
+			if(slow==fast)
+			{
+				flag=false;
+				break;
+			}
+		}
+
+		if(flag)
+		{
+			return null;
+		}
+
+		//as it in loop so 
+		fast=head;//head pointing the same position where they meet 
+		//now I'll move both the pointer with single step 
+
+		while(fast!=slow)
+		{
+			fast=fast.next;
+			slow=slow.next;
+		}
+
+		return fast;
+		
+    }
+}
