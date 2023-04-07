4ed89f0ada0828eaa05108e1d28aa80e3df10d26
Aniket Khangar <116418804+aniket100200@users.noreply.github.com>
2023-04-07 10:49:16 +0530

    Create main.java

---
+++ Module2/LinkedList/Linked Lists 5/Delete Node in Doubly Linked List/main.java
@@ -0,0 +1,120 @@
+import java.util.*;
+
+class Node {
+    int val;
+    Node prev;
+    Node next;
+    Node(int val) {
+        this.val = val;
+        this.prev = null;
+        this.next = null;
+    }
+}
+
+class DoublyLinkedList {
+    Node head;
+    Node tail;
+    int length;
+
+    DoublyLinkedList() {
+        this.head = null;
+        this.tail = null;
+        this.length = 0;
+    }
+
+    void append(int val) {
+        Node newNode = new Node(val);
+        if (this.length == 0) {
+            this.head = newNode;
+            this.tail = newNode;
+        } else {
+            this.tail.next = newNode;
+            newNode.prev = this.tail;
+            this.tail = newNode;
+        }
+        this.length++;
+    }
+
+    void prepend(int val) {
+        Node newNode = new Node(val);
+        if (this.length == 0) {
+            this.head = newNode;
+            this.tail = newNode;
+        } else {
+            this.head.prev = newNode;
+            newNode.next = this.head;
+            this.head = newNode;
+        }
+        this.length++;
+    }
+
+    void delete(Node node) 
+	{
+        // Write your code here
+		if(head==null)
+		{
+			return;
+		}
+		if(node==head && node==tail)
+		{
+			head=null ;
+			tail=null;
+			return;
+		}
+
+		if(node==head)
+		{
+			head=node.next;
+			head.prev=null;
+			return;
+		}
+		//then after I can delete a node from the end or middle 
+
+		node.prev.next=node.next;
+		if(node.next!=null)
+		{
+			node.next.prev=node.prev;
+		}
+		return;
+    }
+
+    void printList() {
+        Node curr = this.head;
+        while (curr != null) {
+            System.out.print(curr.val + " ");
+            curr = curr.next;
+        }
+        System.out.println();
+    }
+}
+
+class Solution{
+    public static DoublyLinkedList deleteNodeWithX(DoublyLinkedList linkedList, int x)
+	{
+        //Write your code here
+		Node temp=linkedList.head;
+		while(temp!=null){
+			if(temp.val==x)
+			{
+				linkedList.delete(temp);
+			}
+			temp=temp.next;
+		}
+		return linkedList;
+    }
+}
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int N= sc.nextInt();
+        int M= sc.nextInt();
+        DoublyLinkedList linkedList = new DoublyLinkedList();
+        for(int i=0; i<N; i++){
+            linkedList.append(sc.nextInt());
+        }
+        Solution Obj = new Solution();
+        linkedList = Obj.deleteNodeWithX(linkedList,M);
+        linkedList.printList();
+
+    }
+}
