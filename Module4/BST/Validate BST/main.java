99287214d52f66dfd261ae34c5032cdfc1e43e3d
Aniket Khangar <116418804+aniket100200@users.noreply.github.com>
2023-04-11 10:49:38 +0530

    Create main.java

---
+++ Module4/BST/Validate BST/main.java
@@ -0,0 +1,89 @@
+/**** you'll have to find the take care of those conditions***/
+//will take O(N) time... because you'll have to go each and every node.. in the BST...
+import java.util.LinkedList; 
+import java.util.Queue; 
+import java.io.*;
+import java.util.*;
+
+class Node{
+    int data;
+    Node left;
+    Node right;
+    Node(int data){
+        this.data = data;
+        left=null;
+        right=null;
+    }
+}
+
+class Main {
+    static Node treeBuilder(String str){  
+        if(str.length()==0 || str.charAt(0)=='-'){
+            return null;
+        }
+        String ip[] = str.split(" ");
+        Node root = new Node(Integer.parseInt(ip[0]));
+        Queue<Node> queue = new LinkedList<>(); 
+        queue.add(root);
+        int i = 1;
+        while(queue.size()>0 && i < ip.length) {
+            Node currNode = queue.peek();
+            queue.remove();
+            String currVal = ip[i];
+            if(!currVal.equals("-1")) {
+                currNode.left = new Node(Integer.parseInt(currVal));
+                queue.add(currNode.left);
+            }
+            i++;
+            if(i >= ip.length)
+                break;
+            currVal = ip[i];
+            if(!currVal.equals("-1")) {
+                currNode.right = new Node(Integer.parseInt(currVal));
+                queue.add(currNode.right);
+            }
+            i++;
+        }
+        
+        return root;
+    }
+    static void printInorder(Node root){
+        if(root == null)
+            return;
+        printInorder(root.left);
+        System.out.print(root.data+" ");
+        printInorder(root.right);
+    }
+    
+  public static void main (String[] args) throws IOException{
+          BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+          int t=Integer.parseInt(br.readLine());
+          while(t > 0){
+              String s = br.readLine();
+            Node root = treeBuilder(s);
+              Solution g = new Solution();
+              if(g.isBST(root))
+              System.out.println("true");
+          else
+              System.out.println("false");
+                t--;
+        }
+    }
+  
+}
+
+class Solution{
+    boolean isBST(Node node)
+	{
+       // Your Code Here
+		return validate(node,Integer.MIN_VALUE,Integer.MAX_VALUE);       
+    }
+	boolean validate(Node root,int l,int r)
+	{
+		if(root==null)return true;
+		if(root.data<l || root.data>r)return false;
+		boolean isLeftBalance=validate(root.left,l,root.data-1);
+		boolean isRightBalacne=validate(root.right,root.data+1,r);
+		return isLeftBalance && isRightBalacne;
+	}
+}
