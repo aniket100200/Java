b51a1de04ce109d867a37e4c4d0d0a50be333e09
Aniket Khangar <116418804+aniket100200@users.noreply.github.com>
2023-04-13 09:23:43 +0530

    Create BrutzForce.java

---
+++ Module4/Prefix Sum & Kadanes/Range Sum Query/Range Sum Query/BrutzForce.java
@@ -0,0 +1,56 @@
+//* in this method you'll take linear time to find the sum got it...
+import java.util.*;
+
+class Solution {
+        public int[] sumQuery(int[] arr, int[][] ranges)
+	{
+				/*
+				* TC:- n*m
+				*where n--> length of arr
+				* m- no.of queries....
+				*/
+            // Your code here.
+		int[] ans=new int[ranges.length];
+		int i=0;
+		for(int[] a:ranges)
+		{
+			int si=a[0];
+			int ei=a[1];
+			int sum=0;
+			while(si<=ei)
+			{
+				sum+=arr[si++];
+			}
+			ans[i++]=sum;
+		}
+		return ans;
+  }
+}
+
+public class Main {
+     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n;
+        n = sc.nextInt();
+        int arr[] = new int[n];
+        for (int i = 0; i < n; i++)
+            arr[i] = sc.nextInt();
+        int m;
+        m = sc.nextInt();
+        int[][] ranges = new int[m][2];
+        for(int i = 0; i < m; i++) {
+            ranges[i][0] = sc.nextInt();
+            ranges[i][1] = sc.nextInt();
+        }
+        Solution Obj = new Solution();
+        int[] ans = Obj.sumQuery(arr, ranges);
+        for(int i =0; i< ans.length; i++)
+            System.out.print(ans[i] + " ");
+    }
+}
+Footer
+© 2023 GitHub, Inc.
+Footer navigation
+Terms
+Privacy
+Security
