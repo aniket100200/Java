bc74be63a07134af498be3ac77f27dad6ba56752
Aniket Khangar <116418804+aniket100200@users.noreply.github.com>
2023-04-24 10:29:55 +0530

    Create main.java

---
+++ Module4/Bit Manipulation/Bit 1/Check whether K-th bit is set or not/main.java
@@ -0,0 +1,36 @@
+import java.io.*;
+import java.util.*;
+
+class Main
+{
+    public static void main(String args[])throws IOException
+    {
+        Scanner sc = new Scanner(System.in);
+        
+        int N = sc.nextInt();
+        int K=sc.nextInt();
+        
+
+        Solution obj=new Solution();
+        Boolean ans=obj.solve(N,K);
+
+        if(ans==true)
+            System.out.println("YES");
+        else
+            System.out.println("NO");
+        
+    }
+}
+
+class Solution{
+    
+    public static Boolean solve(int n,int k)
+    {
+        // Your code here
+		//let's 
+		int bitMask=(1<<k);
+		n=n&bitMask;
+		return n>0;
+		
+    }
+}
