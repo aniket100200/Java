22a8fad3cf43d02d1db56912dc1dad35a1b113cc
Aniket Khangar <116418804+aniket100200@users.noreply.github.com>
2023-05-17 13:05:55 +0530

    Create n2approach.java

---
+++ Module4/Greedy/Greedy 1/Largest permutaion/n2approach.java
@@ -0,0 +1,57 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Solution{
+	class Pair {
+		int val;
+		int idx;
+		public Pair
+	}
+    public static int[] largestPermutation(int[] A, int b)
+	{        
+		//Write your code here
+		int n=A.length;
+		for(int i=0;i<n;i++)
+		{
+			if(b==0)break;
+			int largest=find(A,i+1,i);
+			if(largest==i)continue;
+			int temp=A[i];
+			A[i]=A[largest];
+			A[largest]=temp;
+			b--;
+		}
+		return A;
+        
+    }
+	static int find(int[]arr,int i,int j)
+	{
+		for(;i<arr.length;i++)
+		{
+			if(arr[j]<arr[i])
+			{
+				j=i;
+			}
+		}
+		return j;
+	}
+}
+
+public class Main {
+    public static void main (String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+        int[] A = new int[n];
+        for(int i=0;i<n;i++)
+            A[i] = sc.nextInt();
+
+        int b =sc.nextInt();
+        Solution obj= new Solution();
+        int [] ans = obj.largestPermutation(A, b);
+
+        for(int i=0;i<n;i++)
+            System.out.print(ans[i]+" ");
+    }
+}
